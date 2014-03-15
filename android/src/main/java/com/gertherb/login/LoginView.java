df27ff7d44ac21e19e1c95d3ae85cc38b6176211
Ataul Munim <timberwo7ves@gmail.com>
2014-03-15 16:47:00 +0000

    Adds LoginView with correct assets

--- android/src/main/java/com/gertherb/login/LoginView.java
+++ android/src/main/java/com/gertherb/login/LoginView.java
@@ -2,16 +2,55 @@ package com.gertherb.login;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class LoginView extends FrameLayout {
+import com.gertherb.R;
+import com.novoda.notils.caster.Views;
+
+import rx.Observable;
+import rx.android.observables.ViewObservable;
+import rx.functions.Func1;
+
+public class LoginView extends LinearLayout {
+
+    private EditText username;
+    private EditText password;
+    private Button login;
 
     public LoginView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        inflate();
     }
 
     public LoginView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        inflate();
+    }
+
+    private void inflate() {
+        inflate(getContext(), R.layout.view_login, this);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        username = Views.findById(this, R.id.login_edit_username);
+        password = Views.findById(this, R.id.login_edit_password);
+        login = Views.findById(this, R.id.login_button);
+    }
+
+    public Observable<UserCredentials> newLoginObservable() {
+        return ViewObservable.clicks(login, false).map(new Func1<View, UserCredentials>() {
+
+            @Override
+            public UserCredentials call(View view) {
+                return new UserCredentials(username.getText().toString(), password.getText().toString());
+            }
+
+        });
     }
 
 }
