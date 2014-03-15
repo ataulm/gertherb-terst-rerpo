c656afe4d8ad427c1363fcca38c430c80be7eb89
Ataul Munim <timberwo7ves@gmail.com>
2014-03-15 17:43:34 +0000

    Use LoginView's observable in the Activity

--- android/src/main/java/com/gertherb/login/LoginActivity.java
+++ android/src/main/java/com/gertherb/login/LoginActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 
 import com.gertherb.R;
 import com.gertherb.base.GertHerbActivity;
+import com.novoda.notils.caster.Views;
+import com.novoda.notils.logger.toast.Toaster;
+
+import rx.Observer;
 
 public class LoginActivity extends GertHerbActivity {
 
@@ -11,6 +15,28 @@ public class LoginActivity extends GertHerbActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginView loginView = Views.findById(this, R.id.login_view);
+        loginView.newLoginObservable().subscribe(loginObserver());
+    }
+
+    private Observer<UserCredentials> loginObserver() {
+        return new Observer<UserCredentials>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(UserCredentials userCredentials) {
+                new Toaster(LoginActivity.this).popToast("W0oo0h0o0OOoo");
+            }
+        };
     }
 
 }
