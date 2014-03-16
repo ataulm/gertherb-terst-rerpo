726898f9c5063cbc6d20ee7a542ac97a03cd312f
Antonio Bertucci <archano@gmail.com>
2014-03-16 20:30:56 +0000

    Adds a simple script that retrieves all the current user repos (private and public)

---
+++ api-explorer/src/main/groovy/com/gertherb/api/UserReposScript.groovy
@@ -0,0 +1,37 @@
+package com.gertherb.api
+
+import com.gertherb.api.shared.ApiScript
+import com.gertherb.api.shared.Credentials
+import com.gertherb.api.shared.OAuthCredentials
+import groovyx.net.http.ContentType
+
+class UserReposScript extends ApiScript {
+
+    Credentials credentials = OAuthCredentials.load()
+
+    @Override
+    String description() {
+        return  "List repositories for the authenticated user. Note that this does not include repositories " +
+                "owned by organizations which the user can access. You can list user organizations and list " +
+                "organization repositories separately."
+    }
+
+    @Override
+    String path() {
+        return "/user/repos"
+    }
+
+    @Override
+    ContentType contentType() {
+        return ContentType.JSON
+    }
+
+    @Override
+    def String authorization() {
+        return credentials.getBasicAuthorizationValue()
+    }
+
+    public static void main(String[] args) {
+        new UserReposScript().execute()
+    }
+}
